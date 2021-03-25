8533efb24cc7dd7433d79d42e9634e321c3c0404
Harshit Patil <70749968+HarshitPatil@users.noreply.github.com>
2021-03-25 22:40:29 +0530

    Add files via upload

---
+++ QR-Code_with _python.py
@@ -0,0 +1,24 @@
+import qrcode
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+def encodeqr(data, back_color = "white", color = "black"):
+    qr = qrcode.QRCode(version=1, box_size=50, border=5)
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color = color, back_color = back_color)
+    img.save("myqrcode2.png")
+
+
+def decodeqr(path):
+    image = Image.open(path)
+
+    result = decode(image)
+    print(result) 
+
+
+encodeqr("I am a good Boi!")
+
+decodeqr("myqrcode2.png")
+#during entering path for other images use double back slash or single front slash. 
